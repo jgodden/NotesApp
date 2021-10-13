856add501d8291f67f92c2d0729f9d1abb71c321
Jon Godden <jgodden@hotmail.com>
2021-10-13 16:02:07 +0100

    set domain in callback

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ var strategy = new Auth0Strategy(
     clientID: process.env.AUTH0_CLIENT_ID,
     clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL:
-      process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+      process.env.AUTH0_CALLBACK_URL || 'http://' + process.env.AUTH0_DOMAIN + '/callback'
   },
   function (accessToken, refreshToken, extraParams, profile, done) {
     // accessToken is the token to call Auth0 API (not needed in the most cases)
