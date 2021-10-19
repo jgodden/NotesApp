ee55618cdb3efdc921bda0b1c3d30b8757be40a2
Jon Godden <jgodden@hotmail.com>
2021-10-19 18:12:42 +0100

    add bullet list to tinyMCE tools

--- public/javascripts/noteForm.js
+++ public/javascripts/noteForm.js
@@ -190,6 +190,13 @@ tinymce.init(
 			'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
 			'table emoticons template paste help'
 		  ],
+		toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+		'bullist numlist outdent indent | link image | print preview media fullscreen | ' +
+		'forecolor backcolor emoticons | help',
+		menu: {
+		  favs: {title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons'}
+		},
+		menubar: 'favs file edit view insert format tools table help',
 		content_css: '/stylesheets/style.css',
 	}
 );
